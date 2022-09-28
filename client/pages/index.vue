e9fc4550711fe0b3abdc3e691e1379f31473897b
Galitan-dev <galitan.dev@gmail.com>
2022-09-28 17:26:35 +0200

    chore: initialized a project with nuxt.js and nest.js

---
+++ client/pages/index.vue
@@ -0,0 +1,30 @@
+<!-- client/pages/index.vue -->
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script lang="ts">
+import { Context } from '@nuxt/types';
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'Index'
+})
+export default class Index extends Vue {
+  public hello!: string;
+
+  async asyncData({ $axios, query }: Context) {
+    let hello;
+    try {
+      hello = await $axios.$get('/', { params: query });
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+    }
+
+    return {
+      hello
+    };
+  }
+}
+</script>
